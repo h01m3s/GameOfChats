de192138d44adb4816cdf1f1f45ed2c095d20bd1
h01m3s <lin426797@gmail.com>
2017-06-03 00:59:03 -0400

    Simple email registration using Firebase.

--- GameOfChats/LoginController.swift
+++ GameOfChats/LoginController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginController: UIViewController {
     
@@ -28,8 +29,45 @@ class LoginController: UIViewController {
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
         button.layer.cornerRadius = 5
         button.layer.masksToBounds = true
+        
+        button.addTarget(self, action: #selector(handleRegister), for: .touchUpInside)
         return button
     }()
+    
+    func handleRegister() {
+        
+        guard let email = emailTextField.text, let password = passwordTextField.text, let name = nameTextField.text else {
+            print("Form is not valid")
+            return
+        }
+        
+        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+            if error != nil {
+                print(error ?? "")
+                return
+            }
+            
+            guard let uid = user?.uid else {
+                return
+            }
+            
+            //successfully authenticated user
+            
+            let values = ["name": name, "email": email]
+            let ref = FIRDatabase.database().reference(fromURL: "https://gameofchats-d44a5.firebaseio.com/")
+            let usersReference = ref.child("users").child(uid)
+            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+                if err != nil {
+                    print(err ?? "")
+                    return
+                }
+                
+                print("Saved user successfully into Firebase database")
+                
+            })
+            
+        })
+    }
     
     let nameSeparatorView: UIView = {
        let view = UIView()
